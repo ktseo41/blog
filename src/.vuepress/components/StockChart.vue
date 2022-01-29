a374522d017dc746ec2f50e256eda5debc7ac17f
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-01-30 03:12:56 +0900

    :tada: 기본 증감률 차트 추가

--- src/.vuepress/components/StockChart.vue
+++ src/.vuepress/components/StockChart.vue
@@ -1,34 +1,76 @@
 <template>
-  <g-chart
-    type="ColumnChart"
-    :data="chartData"
-    :options="chartOptions"
-  />
+  <div>
+    <h3>{{ ticker }}</h3>
+    <g-chart
+      type="LineChart"
+      :data="chartData"
+      :options="chartOptions"
+    />
+  </div>
 </template>
 
 <script>
 import { GChart } from 'vue-google-charts'
+import axios from 'axios'
 
 export default {
   name: "StockChart",
   components: {
     GChart
   },
+  props: {
+    ticker: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type: String,
+      required: false
+    },
+    endDate: {
+      type: String,
+      required: false
+    }
+  },
+  created() {
+    (async () => {
+      const { data: stockPrices } = await this.getStockPrices()
+
+      this.chartData = this.getChartData(stockPrices)
+    })();
+  },
+  methods: {
+    async getStockPrices(){
+      const _startDate = this.startDate || new Date(new Date().setDate(new Date().getDate() - 7)).toISOString().slice(0, 10)
+      const _endDate = this.endDate || new Date().toISOString().slice(0,10)
+      const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://yahoo-finance-for-blog.herokuapp.com'
+      const url = baseUrl + `/historical/${this.ticker}?period1=${_startDate}&period2=${_endDate}`
+
+      return await axios.get(url)
+    },
+    getChartData(stockPrices){
+      const data = [
+        ['Date', 'rate']
+      ]
+
+      const { close: firstClose } = stockPrices[0]
+
+      stockPrices.forEach(stockPrice => {
+        const date = new Date(stockPrice.date)
+        const dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+        const closeRate = ((stockPrice.close - firstClose) / firstClose * 100).toFixed(2)
+
+        data.push([dateString, Number(closeRate)])
+      })
+
+      return data
+    },
+  },
   data () {
     return {
-      // Array will be automatically processed with visualization.arrayToDataTable function
-      chartData: [
-        ['Year', 'Sales', 'Expenses', 'Profit'],
-        ['2014', 1000, 400, 200],
-        ['2015', 1170, 460, 250],
-        ['2016', 660, 1120, 300],
-        ['2017', 1030, 540, 350]
-      ],
+      chartData: [],
       chartOptions: {
-        chart: {
-          title: 'Company Performance',
-          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
-        }
+        legend:  { position: 'none' },
       }
     }
   }
