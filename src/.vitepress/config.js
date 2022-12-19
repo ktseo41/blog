4854d158b9177339f5832256349e964e5fe8e314
bohyeon.seo <bohyeon.seo@medistream.co.kr>
2022-12-19 11:21:36 +0900

    :sparkles: sitemap generation 추가

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,6 +1,11 @@
 import { description } from '../../package.json';
+import { SitemapStream } from 'sitemap';
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
 
 // const untilSpring = new Date().getMonth() < 2 || new Date().getMonth() >= 11;
+
+const links = [];
 
 export default {
   base: "/blog/",
@@ -101,4 +106,20 @@ export default {
       },
     ],
   },
+  // sitemap 생성: https://github.com/vuejs/vitepress/issues/520
+  transformHtml: (_, id, { pageData }) => {
+    if (!/[\\/]404\.html$/.test(id))
+      links.push({
+        // you might need to change this if not using clean urls mode
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+        lastmod: pageData.lastUpdated
+      })
+  },
+  buildEnd: ({ outDir }) => {
+    const sitemap = new SitemapStream({ hostname: 'https://ktseo41.github.io/blog/' })
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+    sitemap.pipe(writeStream)
+    links.forEach((link) => sitemap.write(link))
+    sitemap.end()
+  }
 };
