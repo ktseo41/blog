13c6604f809e6ccb8b7657bb2c61c67509aacb46
ktseo41 <ktseo41@gmail.com>
2026-01-19 15:30:12 +0900

    :sparkles: 독서 기록 업데이트

--- src/loaders/reading.data.js
+++ src/loaders/reading.data.js
@@ -2256,16 +2256,37 @@ const bookLogs = [
   {
     name: "달러 자산 1억으로 평생 월급 완성하라",
     startedAt: "2025-12-04",
-    progressValue: (236 / 329) * 100,
+    endAt: "2026-01-19",
+    progressValue: (329 / 329) * 100,
   },
   {
     name: "광기와 우연의 역사",
     startedAt: "2025-12-20",
+    endAt: "2026-01-03",
+    inProgress: false,
     progressValue: (48 / 340) * 100,
     logs: [
       {
         date: "2025-12-20",
         progressValue: (48 / 340) * 100,
+      },
+    ],
+  },
+  {
+    name: "피가 흐르는 곳에",
+    startedAt: "2026-01-11",
+    endAt: "2026-01-25",
+    inProgress: false,
+    progressValue: (1 / 4) * 100,
+  },
+  {
+    name: "자본주의 어디서 와서 어디로 가는가",
+    startedAt: "2026-01-19",
+    progressValue: (24 / 543) * 100,
+    logs: [
+      {
+        date: "2026-01-19",
+        progressValue: (24 / 543) * 100,
       },
     ],
   },
